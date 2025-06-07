670e9cd72f68256a8bd2fb14aa248127b45dcd8a
chibitaka0320 <taka.araat32@gmail.com>
2025-06-07 17:10:23 +0900

    first commit

---
+++ src/test/java/com/chibitaka/tremane_backend/TremaneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chibitaka.tremane_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TremaneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
