352661f276ca591dd2056c2af6b09a354e16b03c
chibitaka0320 <taka.araat32@gmail.com>
2025-06-09 18:54:17 +0900

    リポジトリのインターフェース化

--- src/main/java/com/chibitaka/tremane_backend/repository/UserRepository.java
+++ src/main/java/com/chibitaka/tremane_backend/repository/UserRepository.java
@@ -1,36 +1,18 @@
 package com.chibitaka.tremane_backend.repository;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
-
 import com.chibitaka.tremane_backend.entity.UserEntity;
 import com.chibitaka.tremane_backend.vo.EmailVo;
 
-/** ユーザーテーブル操作りポジトリ */
-@Mapper
+/** ユーザーテーブル操作りポジトリインターフェース */
 public interface UserRepository {
 
         /** ユーザーのID検索 */
-        @Select({
-                        "SELECT user_id, name, email, password, created_at, updated_at",
-                        "FROM users WHERE user_id = #{userId}"
-        })
         UserEntity findById(Long id);
 
         /** ユーザーのメールアドレス検索 */
-        @Select({
-                        "SELECT user_id, name, email, password, created_at, updated_at",
-                        "FROM users WHERE email = #{email}"
-        })
         UserEntity findByEmail(EmailVo email);
 
         /** ユーザー登録 */
-        @Insert({
-                        "INSERT INTO users(email, password)",
-                        "VALUES(#{email}, #{password})"
-        })
-        @Options(useGeneratedKeys = true, keyProperty = "userId")
         int insert(UserEntity record);
+
 }
---
+++ src/main/java/com/chibitaka/tremane_backend/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.chibitaka.tremane_backend.repository.impl;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import com.chibitaka.tremane_backend.entity.UserEntity;
+import com.chibitaka.tremane_backend.repository.UserRepository;
+import com.chibitaka.tremane_backend.vo.EmailVo;
+
+/** ユーザーテーブル操作りポジトリ実装 */
+@Mapper
+public interface UserRepositoryImpl extends UserRepository {
+
+    /** ユーザーのID検索 */
+    @Select({
+            "SELECT user_id, name, email, password, created_at, updated_at",
+            "FROM users WHERE user_id = #{userId}"
+    })
+    UserEntity findById(Long id);
+
+    /** ユーザーのメールアドレス検索 */
+    @Select({
+            "SELECT user_id, name, email, password, created_at, updated_at",
+            "FROM users WHERE email = #{email}"
+    })
+    UserEntity findByEmail(EmailVo email);
+
+    /** ユーザー登録 */
+    @Insert({
+            "INSERT INTO users(email, password)",
+            "VALUES(#{email}, #{password})"
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int insert(UserEntity record);
+}
