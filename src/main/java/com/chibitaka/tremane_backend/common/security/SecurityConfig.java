79e8e2bfae6466009a9f77f772b856b9fdacb490
chibitaka0320 <taka.araat32@gmail.com>
2025-06-09 23:29:21 +0900

    セキュリティフィルター作成

--- src/main/java/com/chibitaka/tremane_backend/common/security/SecurityConfig.java
+++ src/main/java/com/chibitaka/tremane_backend/common/security/SecurityConfig.java
@@ -2,12 +2,28 @@ package com.chibitaka.tremane_backend.common.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /** セキュリティコンフィグクラス */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/signUp").permitAll()
+                .anyRequest().authenticated());
+
+        // CSRF（クロスサイトリクエストフォージェリ）保護を無効化
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
 
     /** パスワードハッシュ化用 */
     @Bean
