e5b33be672af822e3b3353b2ee09f1f8dfab6c8a
chibitaka0320 <taka.araat32@gmail.com>
2025-07-02 20:29:18 +0900

    spring boot バージョン変更

--- src/main/java/com/chibitaka/tremane_backend/TremaneBackendApplication.java
+++ src/main/java/com/chibitaka/tremane_backend/TremaneBackendApplication.java
@@ -2,9 +2,8 @@ package com.chibitaka.tremane_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class TremaneBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/chibitaka/tremane_backend/common/security/JwtAuthenticationFilter.java
+++ src/main/java/com/chibitaka/tremane_backend/common/security/JwtAuthenticationFilter.java
@@ -22,7 +22,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@org.springframework.lang.NonNull HttpServletRequest request,
+            @org.springframework.lang.NonNull HttpServletResponse response,
+            @org.springframework.lang.NonNull FilterChain filterChain)
             throws ServletException, IOException {
         String header = request.getHeader("Authorization");
         if (header != null && header.startsWith("Bearer ")) {
