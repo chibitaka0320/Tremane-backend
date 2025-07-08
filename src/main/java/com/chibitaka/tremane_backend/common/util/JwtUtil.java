7e22a61c0ae9eedc133aafae51a13ecd6530e80f
chibitaka0320 <taka.araat32@gmail.com>
2025-07-08 09:39:25 +0900

    不要ファイル削除

--- src/main/java/com/chibitaka/tremane_backend/common/util/JwtUtil.java
+++ src/main/java/com/chibitaka/tremane_backend/common/util/JwtUtil.java
@@ -1,14 +1,9 @@
 package com.chibitaka.tremane_backend.common.util;
 
-import java.util.Date;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.chibitaka.tremane_backend.common.error.AuthenticationException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
@@ -19,41 +14,6 @@ import com.google.firebase.auth.FirebaseToken;
 @ConfigurationProperties(prefix = "jwt")
 public class JwtUtil {
 
-    @SuppressWarnings("unused")
-    private String secretKey;
-    private Algorithm algorithm;
-
-    public void setSecretKey(String secretKey) {
-        this.secretKey = secretKey;
-        this.algorithm = Algorithm.HMAC256(secretKey); // setterが呼ばれた後に初期化
-    }
-
-    /** アクセストークン作成 */
-    public String createAccessToken(String userId) {
-        try {
-            String token = JWT.create()
-                    .withSubject(String.valueOf(userId))
-                    .withExpiresAt(new Date(System.currentTimeMillis() + 60 * 60 * 1000)) // 1時間
-                    .sign(algorithm);
-            return token;
-        } catch (JWTVerificationException e) {
-            throw new AuthenticationException(HttpStatus.UNAUTHORIZED.value(), null, e.getMessage());
-        }
-    }
-
-    /** リフレッシュトークン作成 */
-    public String createRefreshToken(String userId) {
-        try {
-            String token = JWT.create()
-                    .withSubject(String.valueOf(userId))
-                    .withExpiresAt(new Date(System.currentTimeMillis() + 525600 * 60 * 1000)) // 1年間
-                    .sign(algorithm);
-            return token;
-        } catch (JWTVerificationException e) {
-            throw new AuthenticationException(HttpStatus.UNAUTHORIZED.value(), null, e.getMessage());
-        }
-    }
-
     /** ユーザーIDの抽出 */
     public String extractUserId(String token) {
         try {
@@ -61,16 +21,6 @@ public class JwtUtil {
             return decodedToken.getUid();
         } catch (FirebaseAuthException e) {
             throw new AuthenticationException(HttpStatus.UNAUTHORIZED.value(), "10005E", e.getMessage());
-        }
-    }
-
-    /** リフレッシュトークンの検証 */
-    public boolean validateRefreshToken(String token) {
-        try {
-            JWT.require(algorithm).build().verify(token);
-            return true;
-        } catch (JWTVerificationException e) {
-            return false;
         }
     }
 }
