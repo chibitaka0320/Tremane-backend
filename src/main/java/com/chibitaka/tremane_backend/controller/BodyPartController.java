5e6356b1b5099271e4053bad1dd2989e8fc167f3
chibitaka0320 <taka.araat32@gmail.com>
2025-07-25 17:31:38 +0900

    bodypartcontrller fix

--- src/main/java/com/chibitaka/tremane_backend/controller/BodyPartController.java
+++ src/main/java/com/chibitaka/tremane_backend/controller/BodyPartController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chibitaka.tremane_backend.dto.BodyPartDto;
-import com.chibitaka.tremane_backend.dto.BodyPartExerciseDto;
 import com.chibitaka.tremane_backend.service.BodyPartService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,12 +22,6 @@ public class BodyPartController {
     private final BodyPartService bodyPartService;
 
     @GetMapping("")
-    public ResponseEntity<List<BodyPartExerciseDto>> getBodyPartExerciseList() {
-        List<BodyPartExerciseDto> bodyPartExerciseDto = bodyPartService.getBodyPartList();
-        return ResponseEntity.ok(bodyPartExerciseDto);
-    }
-
-    @GetMapping("/sync")
     public ResponseEntity<List<BodyPartDto>> getBodyParts(@RequestParam LocalDateTime updatedAt) {
         List<BodyPartDto> bodyPartDtos = bodyPartService.getBodyPartList(updatedAt);
         return ResponseEntity.ok(bodyPartDtos);
