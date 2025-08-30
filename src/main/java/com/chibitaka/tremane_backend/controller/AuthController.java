7656a5be684ae3b010616bd52f76ade51813b342
chibitaka0320 <taka.araat32@gmail.com>
2025-08-30 09:41:31 +0900

    匿名カスタムトークン発行処理作成

--- src/main/java/com/chibitaka/tremane_backend/controller/AuthController.java
+++ src/main/java/com/chibitaka/tremane_backend/controller/AuthController.java
@@ -1,14 +1,19 @@
 package com.chibitaka.tremane_backend.controller;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chibitaka.tremane_backend.form.SignUpForm;
 import com.chibitaka.tremane_backend.service.SignUpService;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,5 +34,16 @@ public class AuthController {
     public ResponseEntity<Void> signUp(@Validated @RequestBody SignUpForm form) {
         signUpService.signUp(form);
         return ResponseEntity.status(204).build();
+    }
+
+    /** 匿名ユーザーカスタムトークン発行 */
+    @PostMapping("/reauth_token")
+    public Map<String, String> issueReauthToken(@RequestHeader("Authorization") String bearer) throws Exception {
+        String idToken = bearer.replace("Bearer ", "");
+
+        FirebaseToken decoded = FirebaseAuth.getInstance().verifyIdToken(idToken);
+        String uid = decoded.getUid();
+        String customToken = FirebaseAuth.getInstance().createCustomToken(uid);
+        return Map.of("customToken", customToken);
     }
 }
